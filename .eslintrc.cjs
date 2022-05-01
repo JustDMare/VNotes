392c923b6dd1fd8eda1a19193e882cc1fddff985
JustDMare <UO264608@uniovi.es>
2022-05-02 00:41:34 +0200

    Finished initial setut ESLint configured

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,24 +2,35 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
+  root: true,
+  extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier"
+    "@vue/eslint-config-prettier",
   ],
-  "env": {
-    "vue/setup-compiler-macros": true
+  env: {
+    "vue/setup-compiler-macros": true,
   },
-  "overrides": [
+  overrides: [
     {
-      "files": [
-        "cypress/integration/**.spec.{js,ts,jsx,tsx}"
-      ],
-      "extends": [
-        "plugin:cypress/recommended"
-      ]
-    }
-  ]
-}
+      files: ["cypress/integration/**.spec.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
+    },
+  ],
+  rules: {
+    curly: ["error", "all"],
+    eqeqeq: ["error", "always", { null: "ignore" }],
+    "max-len": [
+      "error",
+      { code: 100, ignoreComments: false, ignorePattern: 'd="([\\s\\S]*?)"' },
+    ],
+    "no-dupe-class-members": "off",
+    "no-duplicate-imports": "error",
+    "no-empty-function": "error",
+    "require-await": "error",
+
+    //VUE
+    "vue/eqeqeq": "error",
+  },
+};
