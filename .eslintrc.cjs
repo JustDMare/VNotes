4cdee528e6453a2c02f0020e103b3dbedd0178b9
JustDMare <UO264608@uniovi.es>
2022-05-02 18:52:22 +0200

    Added support for SCSS and accesibility lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,15 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  plugins: ["vuejs-accessibility"],
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vuejs-accessibility/recommended",
   ],
+
   env: {
     "vue/setup-compiler-macros": true,
   },
