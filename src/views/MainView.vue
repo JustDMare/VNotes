9a506d24fc4466687673f7304d3938b9990df239
JustDMare <UO264608@uniovi.es>
2022-05-08 20:25:52 +0200

    Reorganized components. Created main layout

---
+++ src/views/MainView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import EditorComponent from "@/components/editor/EditorComponent.vue";
+</script>
+
+<template>
+  <main>
+    <EditorComponent></EditorComponent>
+  </main>
+</template>
