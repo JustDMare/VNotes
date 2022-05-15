3b50eba52280261a2b9970ce2e9874a36ccf6d9d
Daniel Adrian Mare <UO264608@uniovi.es>
2022-05-15 17:58:10 +0200

    Added title to note. Added Roboto font as default (#2)

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,10 @@ import { RouterView } from "vue-router";
 </template>
 
 <style>
-@import "@/assets/base.css";
+@import "@/assets/base.scss";
 
 #app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+  
 }
 
 header {
