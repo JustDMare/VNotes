3c960e536a05ff0333b6e6a1fa02c724ca65084b
JustDMare <UO264608@uniovi.es>
2022-08-23 22:46:41 +0200

    WIP: Added text block to BlockList for testing

--- src/components/editor/block-list.vue
+++ src/components/editor/block-list.vue
@@ -1,5 +1,8 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import TextBlock from "./blocks/text-block.vue";
+</script>
 
 <template>
   <div>List of blocks</div>
+  <TextBlock></TextBlock>
 </template>
