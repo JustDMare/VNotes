05605f5954b40a8b2ffb79e08a28e0034cde7474
Daniel Adrian Mare <UO264608@uniovi.es>
2022-09-24 12:55:06 +0200

    feat: Created button for adding new blocks (#7)

--- src/components/editor/blocks/BlockComponent.vue
+++ src/components/editor/blocks/BlockComponent.vue
@@ -1,16 +1,28 @@
 BLOCK_DICTIONARYBLOCK_DICTIONARY
 <script setup lang="ts">
 import BLOCK_DICTIONARY from "@/services/block-dictionary";
+import { useMainStore } from "@/stores/main";
 import type { Block } from "@/types/blocks";
 import type { PropType } from "vue";
 
-defineProps({
+const props = defineProps({
   block: { type: Object as PropType<Block>, required: true },
 });
+const mainStore = useMainStore();
+
+function createBlockBelow() {
+  mainStore.createBlockInEditor(props.block.blockId);
+}
 </script>
 
 <template>
-  <component :is="BLOCK_DICTIONARY.get(block.type)" :block="block"></component>
+  <div>
+    <button @click="createBlockBelow">+</button>
+    <component
+      :is="BLOCK_DICTIONARY.get(block.type)"
+      :block="block"
+    ></component>
+  </div>
 </template>
 
 <style scoped>
