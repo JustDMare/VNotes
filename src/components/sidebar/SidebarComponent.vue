1d9f8f327f0e8b0588f2fff39e7d5d14ac519c69
JustDMare <just@dmare.me>
2022-10-21 19:23:39 +0200

    feat: added resizer to the sidebar

--- src/components/sidebar/SidebarComponent.vue
+++ src/components/sidebar/SidebarComponent.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
 import { useMainStore } from "@/stores/main";
+import { ref, type Ref } from "vue";
 import SidebarFolder from "./SidebarFolder.vue";
 import SidebarNote from "./SidebarNote.vue";
 
 const mainStore = useMainStore();
 const sidebarContent = mainStore.$state.workspace.content;
-console.log(sidebarContent);
+const sidebar: Ref<HTMLDivElement | null> = ref(null);
+defineExpose({ sidebar });
 </script>
 
 <template>
-  <div>
-    <SidebarFolder
-      v-for="folderReference in sidebarContent.folders"
-      :key="folderReference.folderID"
-      :folder-reference="folderReference"
-    />
-    <SidebarNote
-      v-for="noteReference in sidebarContent.notes"
-      :key="noteReference.noteID"
-      :note-reference="noteReference"
-    />
-  </div>
+  <aside ref="sidebar">
+    <nav>
+      <SidebarFolder
+        v-for="folderReference in sidebarContent.folders"
+        :key="folderReference.folderID"
+        :folder-reference="folderReference"
+      />
+      <SidebarNote
+        v-for="noteReference in sidebarContent.notes"
+        :key="noteReference.noteID"
+        :note-reference="noteReference"
+      />
+    </nav>
+  </aside>
 </template>
 
 <style scoped lang="scss"></style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,17 +1,105 @@
 <script setup lang="ts">
 import EditorComponent from "@/components/editor/EditorComponent.vue";
 import SidebarComponent from "@/components/sidebar/SidebarComponent.vue";
+import { onMounted, ref, type Ref } from "vue";
+
+const sidebarComponent: Ref<InstanceType<typeof SidebarComponent> | null> =
+  ref(null);
+const editorComponent: Ref<InstanceType<typeof EditorComponent> | null> =
+  ref(null);
+const resizer: Ref<HTMLDivElement | null> = ref(null);
+
+function resize(event: MouseEvent): void {
+  const size = `${event.x}px`;
+  if (sidebarComponent.value && sidebarComponent.value.sidebar) {
+    sidebarComponent.value.sidebar.style.flexBasis = size;
+    sidebarComponent.value.sidebar.style.cursor = "col-resize";
+  }
+  if (editorComponent.value && editorComponent.value.editor) {
+    editorComponent.value.editor.classList.toggle("moving", true);
+  }
+}
+
+onMounted(() => {
+  if (resizer.value) {
+    resizer.value.addEventListener("mousedown", () => {
+      document.addEventListener("mousemove", resize, false);
+      document.addEventListener(
+        "mouseup",
+        () => {
+          document.removeEventListener("mousemove", resize, false);
+          if (sidebarComponent.value && sidebarComponent.value.sidebar) {
+            sidebarComponent.value.sidebar.style.cursor = "default";
+          }
+          if (editorComponent.value && editorComponent.value.editor) {
+            editorComponent.value.editor.classList.toggle("moving", false);
+          }
+        },
+        false
+      );
+    });
+  }
+});
 </script>
 
 <template>
-  <div id="editor">
-    <SidebarComponent id="sidebar" />
-    <EditorComponent id="editor" />
-  </div>
+  <section id="main-view">
+    <SidebarComponent id="sidebarComponent" ref="sidebarComponent" />
+    <div class="resizer" ref="resizer">
+      <span class="resizer-delimiter"></span>
+    </div>
+    <EditorComponent id="editorComponent" ref="editorComponent" />
+  </section>
 </template>
+
 <style lang="scss" scoped>
-#editor {
+#main-view {
   display: flex;
   flex-direction: row;
+  height: 100%;
+}
+
+#sidebarComponent {
+  height: 100%;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  width: 260px;
+  min-width: 220px;
+  max-width: 400px;
+}
+
+.resizer {
+  flex-basis: 1px;
+  z-index: 2;
+  cursor: col-resize;
+  padding: 0 8px;
+  box-sizing: border-box;
+
+  &-delimiter {
+    border-right: 1px solid rgba(0, 0, 0, 0.4);
+    height: 100%;
+    display: block;
+    margin-left: -4px;
+  }
+}
+
+#editorComponent {
+  flex-basis: 0;
+  flex-grow: 1;
+  height: 100%;
+  flex-direction: row;
+  position: relative;
+  display: flex;
+  margin: 0;
+  padding: 0;
+
+  &.moving {
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
+  }
 }
 </style>
