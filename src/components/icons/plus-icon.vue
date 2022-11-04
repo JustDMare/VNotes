45dae61e15f7501644898902b76398ae77a81156
JustDMare <just@dmare.me>
2022-11-04 12:20:58 +0100

    feat: Added plus icon component

---
+++ src/components/icons/plus-icon.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+/**
+ * Plus icon belonging to Lucide icon set ({@link https://lucide.dev}).
+ * Obtained via Iconify ({@link https://iconify.design/})
+ */
+export default {};
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+    <line x1="12" y1="5" x2="12" y2="19"></line>
+    <line x1="5" y1="12" x2="19" y2="12"></line>
+  </svg>
+</template>
