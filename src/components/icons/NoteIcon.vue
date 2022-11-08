2375a436650c52e6e029bae8c1add3982961218a
JustDMare <just@dmare.me>
2022-11-08 13:11:49 +0100

    feat: Added new icons and changed naming convention

---
+++ src/components/icons/NoteIcon.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+/**
+ * File icon belonging to Lucide icon set ({@link https://lucide.dev}).
+ * Obtained via Iconify ({@link https://icon-sets.iconify.design/lucide/file/})
+ */
+export default {};
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <g
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    >
+      <path
+        d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"
+      />
+      <path d="M14 2v6h6" />
+    </g>
+  </svg>
+</template>
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,2 +1,5 @@
-export { default as PlusIcon } from "./plus-icon.vue";
-export { default as GripIcon } from "./grip-icon.vue";
+export { default as PlusIcon } from "./PlustIcon.vue";
+export { default as GripIcon } from "./GripIcon.vue";
+export { default as FolderIcon } from "./FolderIcon.vue";
+export { default as ChevronRightIcon } from "./ChevronRightIcon.vue";
+export { default as NoteIcon } from "./NoteIcon.vue";
