a5ce43cc3f4343515021a839582d661f96160e22
JustDMare <just@dmare.me>
2023-01-15 12:59:58 +0100

    Added icons for command palette

---
+++ src/components/icons/HeadingMediumIcon.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+/**
+ * Heading-2 icon belonging to Lucide icon set ({@link https://lucide.dev}).
+ * Obtained via Iconify ({@link https://icon-sets.iconify.design/lucide/heading-2/})
+ */
+export default {};
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 12h8m-8 6V6m8 12V6m9 12h-4c0-4 4-3 4-6c0-1.5-2-2.5-4-1"
+    />
+  </svg>
+</template>
---
+++ src/components/icons/PlainTextIcon.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+/**
+ * Baseline icon belonging to Lucide icon set ({@link https://lucide.dev}).
+ * Obtained via Iconify ({@link https://icon-sets.iconify.design/lucide/baseline/})
+ */
+export default {};
+</script>
+
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M4 20h16M6 16l6-12l6 12M8 12h8"
+    />
+  </svg>
+</template>
