f2ddde0941bf1d2358f872354905f34dc411b6bb
JustDMare <just@dmare.me>
2023-01-20 21:51:49 +0100

    Styled drag and drop blocks

--- src/components/editor/BlockList.vue
+++ src/components/editor/BlockList.vue
@@ -19,8 +19,6 @@ const options = { handle: ".handle" };
 
 function onEnd(event: Sortable.SortableEvent) {
   if (event.oldIndex !== undefined && event.newIndex !== undefined) {
-    console.log("Hi");
-    console.log(event.oldIndex, event.newIndex);
     editorStore.moveBlockInNote(event.oldIndex, event.newIndex);
   }
 }
@@ -28,10 +26,10 @@ function onEnd(event: Sortable.SortableEvent) {
 
 <template>
   <SortableComponent
-    class="a"
     :list="blockList"
     item-key="_id"
     tag="div"
+    group="blocks"
     :key="editorStore.noteInEditor?._id"
     :options="options"
     @end="onEnd"
@@ -41,3 +39,23 @@ function onEnd(event: Sortable.SortableEvent) {
     </template>
   </SortableComponent>
 </template>
+
+<style scoped lang="scss">
+.sortable-chosen {
+  background-color: var(--color-base-90);
+  border-radius: 3px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  opacity: 0.7;
+  transition: all 0.2s ease;
+}
+
+.sortable-ghost {
+  background-color: var(--color-base-70);
+  opacity: 0.5;
+  border: 1px solid var(--color-base-40);
+  padding-top: 4px;
+  padding-bottom: 4px;
+  border-radius: 3px;
+}
+</style>
