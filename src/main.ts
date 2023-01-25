c229ba761d12bf26bcd3ea600685e5605875be1d
JustDMare <just@dmare.me>
2023-01-25 19:35:30 +0100

    Added the auth0 plugin

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
 import App from "./App.vue";
-import router from "./router";
+import { createRouter } from "./router";
 import { i18n } from "./i18n/i18n.plugin";
+import { createAuth0 } from "@auth0/auth0-vue";
 
 const app = createApp(App);
 app.use(createPinia());
+const router = createRouter(app);
 app.use(router);
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+    },
+  })
+);
 app.use(i18n);
 
 app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,27 +1,28 @@
 import { useEditorStore } from "@/stores/editor";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter as createVueRouter, createWebHistory, type Router } from "vue-router";
 import MainView from "@/views/MainView.vue";
+import type { App } from "vue";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: MainView,
-    },
-    {
-      path: "/:id",
-      name: "editor",
-      component: MainView,
-      beforeEnter(to) {
-        const editorStore = useEditorStore();
-        const noteId = <string>to.params.id;
-
-        editorStore.fetchNote(noteId);
+export function createRouter(app: App): Router {
+  return createVueRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: "/",
+        name: "home",
+        component: MainView,
       },
-    },
-  ],
-});
+      {
+        path: "/:id",
+        name: "editor",
+        component: MainView,
+        beforeEnter(to) {
+          const editorStore = useEditorStore();
+          const noteId = <string>to.params.id;
 
-export default router;
+          editorStore.fetchNote(noteId);
+        },
+      },
+    ],
+  });
+}
