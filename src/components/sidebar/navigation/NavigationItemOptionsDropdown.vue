c14d4f67f15b9d60fb63f13306334d23d079e749
JustDMare <just@dmare.me>
2023-01-30 20:21:57 +0100

    Implemented NavigationItemOptionsDropdown

---
+++ src/components/sidebar/navigation/NavigationItemOptionsDropdown.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import BaseDropdown from "@/components/base/BaseDropdown.vue";
+import { OptionsButtonIcon } from "@/components/icons";
+import type { NavigationItemOption } from "@/types";
+import type { PropType } from "vue";
+
+defineProps({
+  options: {
+    type: Array as PropType<NavigationItemOption[]>,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <BaseDropdown class="nav-item_options">
+    <template #button-content>
+      <OptionsButtonIcon
+        :title="$t('tooltips.navigationItemOptionsButton')"
+        class="nav-item_options__icon"
+      />
+    </template>
+    <template #menu>
+      <button v-for="option in options" :key="option.name" @click="option.action">
+        <component :is="option.icon" :title="option.name" />
+        <span>{{ option.name }}</span>
+      </button>
+    </template>
+  </BaseDropdown>
+</template>
+
+<style lang="scss" scoped></style>
