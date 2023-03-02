b2ae9b970ce365ac095a3c7c63118560883e1155
JustDMare <just@dmare.me>
2023-03-02 18:24:04 +0100

    Added chevron rotating logic to its component

--- src/components/dialogs/move-item/MoveItemDialogTargetFolder.vue
+++ src/components/dialogs/move-item/MoveItemDialogTargetFolder.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { Folder } from "vnotes-types";
+import { ref } from "vue";
 import MoveItemDialogTargetFolderButton from "./MoveItemDialogTargetFolderButton.vue";
 
 defineProps<{ folder: Folder; selectedNewParentFolderId: string | null }>();
@@ -7,13 +8,22 @@ const emits = defineEmits<{
   (e: "folder-selected", folderId: string | null): void;
 }>();
 
+const showSubfolders = ref(false);
+
 function handleFolderSelected(folderId: string | null) {
   emits("folder-selected", folderId);
+}
+
+function toggleSubfolderVisibility() {
+  showSubfolders.value = !showSubfolders.value;
 }
 </script>
 
 <template>
   <li>
+    <button @click="toggleSubfolderVisibility">
+      <ChevronRightIcon class="nav__icon nav__icon--chevron" :rotate-down="showSubfolders" />
+    </button>
     <MoveItemDialogTargetFolderButton
       :folder-name="folder.name"
       :folder-id="folder._id"
