51f20d447a3ef3db9ba874c1ac4a08da9ce8dd6a
JustDMare <just@dmare.me>
2023-03-31 20:03:21 +0200

    WIP: Landing page

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -11,9 +11,52 @@ function login() {
 </script>
 
 <template>
-  <div>
+  <header>
+    <h1>VNotes</h1>
     <button @click="login">{{ $t("auth.login") }}</button>
-  </div>
+  </header>
+  <main>
+    <p>
+      VNotes is an intuitive and minimalistic writing app that draws inspiration from widely-used
+      productivity tools like Notion, Craft, and Obsidian. The primary goal of VNotes is to provide
+      users with a streamlined UI/UX, and focusing on simplicity and user-friendliness.
+    </p>
+    <p>
+      VNotes was developed by Daniel Adrian Mare as part of his Final Degree Project at the
+      University of Oviedo under the guidance of tutor José Manuel Redondo López.
+    </p>
+    <img src="@/assets/landing-image.png" alt="screenshot of the application" />
+    <h2>Licenses</h2>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  box-shadow: -8px 13px 43px 0px rgba(0, 0, 0, 0.03);
+  background-color: var(--color-base-100);
+  padding: 12px;
+  h1 {
+    grid-column: 2/2;
+    justify-self: center;
+  }
+  button {
+    grid-column: 3/3;
+  }
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  max-width: 900px;
+  margin: auto;
+  gap: 12px;
+
+  img {
+    max-width: 900px;
+    align-self: center;
+  }
+}
+</style>
