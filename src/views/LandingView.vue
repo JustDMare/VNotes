9459b5da7e9ce2be3ba3be5d47cc64f0671f698e
JustDMare <just@dmare.me>
2023-04-03 20:09:38 +0200

    WIP: Improvements

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -26,58 +26,37 @@ function login() {
     <main>
       <section class="landing__body">
         <p class="landing__body__paragraph">
-          VNotes is an intuitive and minimalistic writing app that draws inspiration from
-          widely-used productivity tools like Notion, Craft, and Obsidian. The primary goal of
-          VNotes is to provide users with a streamlined UI/UX, and focusing on simplicity and
-          user-friendliness.
+          {{ $t("landingPage.appDescription") }}
         </p>
         <p class="landing__body__paragraph">
-          VNotes was developed by Daniel Adrian Mare as part of his Final Degree Project at the
-          University of Oviedo under the guidance of tutor José Manuel Redondo López.
+          {{ $t("landingPage.developedBy") }}
         </p>
-        <img src="@/assets/landing-image.png" alt="screenshot of the application" />
-        <h2 class="landing__body__title2">Technologies</h2>
+        <img src="@/assets/landing-image.png" :alt="$t('landingPage.appScreenshotAlt')" />
+        <h2 class="landing__body__title2">{{ $t("landingPage.technologies.title") }}</h2>
         <p class="landing__body__paragraph">
-          <span>
-            The application has been divided into a traditional frontend-backend architecture using
-            the technologies listed below. For a more comprehesive list on all the third party
-            packages and their licensing, visit the
-          </span>
-          <a href="https://github.com/JustDMare/VNotes/blob/main/NOTICE">frontend NOTICE file </a>
-          <span> and the </span>
-          <a href="https://github.com/JustDMare/VNotes-Server/blob/main/NOTICE"
-            >backend NOTICE file</a
-          >
+          <span> {{ $t("landingPage.technologies.notice.part1") }}</span>
+          <a href="https://github.com/JustDMare/VNotes/blob/main/NOTICE">
+            {{ $t("landingPage.technologies.notice.part2") }}
+          </a>
+          <span>{{ $t("landingPage.technologies.notice.part3") }}</span>
+          <a href="https://github.com/JustDMare/VNotes-Server/blob/main/NOTICE">
+            {{ $t("landingPage.technologies.notice.part4") }}
+          </a>
+          <span>{{ $t("landingPage.technologies.notice.part5") }}</span>
         </p>
-        <h3 class="landing__body__title3">Frontend</h3>
+        <h3 class="landing__body__title3">{{ $t("landingPage.technologies.frontend.title") }}</h3>
         <ul>
-          <li>Vue.js 3: Component library used to build the front of the application.</li>
-          <li>
-            Typescript: Used for type checking across the front and for backend-frontend
-            communication.
+          <li v-for="(item, index) in $tm('landingPage.technologies.frontend.list')" :key="index">
+            <span>{{ item.name }}: </span>{{ item.description }}
           </li>
-          <li>Vue-router: Vue's solution for routing</li>
-          <li>Vue-i18n: Internationalization library for Vue applications.</li>
-          <li>Pinia: State management library created by the Vue team.</li>
-          <li>
-            Sortablejs-vue: Library to easily implement drag n' drop sorting. Derived from
-            Sortablejs and made for Vue 3 apps.
-          </li>
-          <li>Match-sorter: Enables simple search and sort functionalities.</li>
         </ul>
-        <p>Icons obtained via the Lucide icon set</p>
-        <h3 class="landing__body__title3">Backend</h3>
-        <p class="landing__body__paragraph">
-          The application uses Auth0 for authentication and authorization, and MongoDB Atlas for the
-          database
-        </p>
+        <p>{{ $t("landingPage.technologies.frontend.iconSet") }}</p>
+        <h3 class="landing__body__title3">
+          {{ $t("landingPage.technologies.backend.title") }}
+        </h3>
         <ul>
-          <li>Node.js v16: Runtime that allows the use of Javascript for backend development.</li>
-          <li>Express.js: Lightweight framework used for the creation of the backend API</li>
-          <li>Mongoose: ODM library used to interact with MongoDB from the server</li>
-          <li>
-            Typescript: Used mainly to typecheck the Mongoose models and make sure they are
-            compatible with the frontend
+          <li v-for="(item, index) in $tm('landingPage.technologies.backend.list')" :key="index">
+            <span>{{ item.name }}: </span>{{ item.description }}
           </li>
         </ul>
       </section>
@@ -86,76 +65,80 @@ function login() {
 </template>
 
 <style lang="scss" scoped>
-article {
+.landing {
   display: flex;
   flex-direction: column;
   margin: auto;
   height: 100%;
   overflow-y: auto;
-}
-header {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  &__header {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
 
-  justify-items: end; /* Aligns grid items to the start (left) */
-  align-items: center; /* Aligns grid items to the center (vertically) */
-  box-shadow: -8px 13px 43px 0px rgba(0, 0, 0, 0.03);
+    justify-items: end; /* Aligns grid items to the start (left) */
+    align-items: center; /* Aligns grid items to the center (vertically) */
+    box-shadow: -8px 13px 43px 0px rgba(0, 0, 0, 0.03);
 
-  background-color: var(--color-base-100);
-  padding: 12px;
-  h1 {
-    grid-column: 2/2;
-    justify-self: center;
+    background-color: var(--color-base-100);
+    padding: 12px;
+    &__lang-selector {
+      grid-column: 1/1;
+      justify-self: start;
+    }
+    h1 {
+      grid-column: 2/2;
+      justify-self: center;
+    }
+    button {
+      grid-column: 3/3;
+      font-size: 16px;
+      justify-self: end;
+      //Could be turned into a button--primary class
+      justify-content: center;
+      display: flex;
+      align-items: center;
+      background: var(--color-base-100);
+      color: var(--color-text);
+      border: 1px solid var(--color-base-80);
+      border-radius: 4px;
+      gap: 6px;
+      padding: 6px;
+      cursor: pointer;
+      transition: all 0.2s ease-in-out;
+      &:hover {
+        background: var(--color-base-80);
+        color: var(--x=color-base-0);
+      }
+      &:active {
+        transform: scale(0.95);
+      }
+      span {
+        font-size: inherit;
+      }
+      svg {
+        width: 16px;
+        height: 16px;
+        align-self: center;
+      }
+    }
   }
-  button {
-    grid-column: 3/3;
-    font-size: 16px;
-    align-self: center;
-    //Could be turned into a button--primary class
-    justify-content: center;
-    display: flex;
-    align-items: center;
-    background: var(--color-base-100);
-    color: var(--color-text);
-    border: 1px solid var(--color-base-80);
-    border-radius: 4px;
-    gap: 6px;
-    padding: 6px;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    &:hover {
-      background: var(--color-base-80);
-      color: var(--x=color-base-0);
+
+  main {
+    height: 100%;
+    width: 100%;
+    overflow-y: auto;
+    section {
+      display: flex;
+      flex-direction: column;
+      padding: 12px;
+      max-width: 900px;
+      margin: auto;
+      gap: 12px;
     }
-    &:active {
-      transform: scale(0.95);
-    }
-    span {
-      font-size: inherit;
-    }
-    svg {
-      width: 16px;
-      height: 16px;
+    img {
+      max-width: 900px;
       align-self: center;
     }
-  }
-}
-
-main {
-  height: 100%;
-  width: 100%;
-  overflow-y: auto;
-  section {
-    display: flex;
-    flex-direction: column;
-    padding: 12px;
-    max-width: 900px;
-    margin: auto;
-    gap: 12px;
-  }
-  img {
-    max-width: 900px;
-    align-self: center;
   }
 }
 </style>
