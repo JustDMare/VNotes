225c5dc551466ad9dd605d382d86d4a419985a40
JustDMare <just@dmare.me>
2023-06-06 00:07:57 +0200

    Minor change

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import { RouterView } from "vue-router";
 import { useEditorStore } from "./stores/editor";
 import { useUserSpaceStore } from "./stores/user-space";
+import { useEventStore } from "./stores/event";
 
 useUserSpaceStore(); //Needed so the store is initialized\
 useEditorStore(); //Needed so the store is initialized
+useEventStore(); //Needed so the store is initialized
 </script>
 
 <template>
