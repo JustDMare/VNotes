9ad094e8457166fd81a8eda751b6a20da5a200bb
JustDMare <just@dmare.me>
2023-06-06 23:06:21 +0200

    Fixed Cypress. Added command to log in

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,14 +1,6 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
-  });
-});
-
 describe("First Test", () => {
   it("Visits the landing page", () => {
-    cy.visit("http://localhost:3000");
-    cy.contains("Sign in");
-    cy.get(".landing__header__login-btn").click();
-    cy.url().should("include", "/commands/actions");
+    // Login on Auth0.
+    cy.loginWithAuth0(Cypress.env("auth0_username"), Cypress.env("auth0_password"));
   });
 });
